3512fdaed99c707ef29727fb4ecd3aeec8745a8b
Ping Chen <remotenonsense@gmail.com>
2019-09-18 16:35:18 +0900

    Use current color as background color

--- examples/src/Examples/Simple.purs
+++ examples/src/Examples/Simple.purs
@@ -9,6 +9,7 @@ import Data.Symbol (SProxy(..))
 import Effect.Aff.Class (class MonadAff)
 import Halogen as H
 import Halogen.ColorPicker as CP
+import Halogen.ColorPicker.Util as U
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
 
@@ -30,17 +31,20 @@ style = HP.attr (HH.AttrName "style")
 
 render :: forall m. MonadAff m => State -> H.ComponentHTML Action Slot m
 render state =
-  HH.div_
-  [ HH.h1_
-    [ HH.text "Simple color picker" ]
-  , HH.div_
-    [ HH.div
-      [ style "display: inline-block; overflow: hidden; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)"]
-      [ HH.slot _picker unit CP.component state.color $ Just <<< HandleColorPicker ]
+  HH.div
+  [ style $ "min-height: 100vh; display: flex; align-items: center; justify-content: center; background-color: " <> U.toHexString state.color
+  ]
+  [ HH.div
+    [ style "background-color: white; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08);"]
+    [ HH.div_
+      [ HH.div
+        [ style "display: inline-block; overflow: hidden;"]
+        [ HH.slot _picker unit CP.component state.color $ Just <<< HandleColorPicker ]
+      ]
+    , HH.div
+      [ style "padding: 1.5rem; text-align: center; border-top: 1px solid lightgray;" ]
+      [ HH.text $ "Current color is: " <> U.toHexString state.color ]
     ]
-  , HH.div
-    [ style "margin-top: 2rem;" ]
-    [ HH.text $ "Current color is: " <> show state.color ]
   ]
 
 initialState :: State
